f15d435c5c4179d6cf735adb47b597db25b0abbe
origami-matrix <71522316+origami-matrix@users.noreply.github.com>
2021-03-15 20:13:06 -0400

    added some basic files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import * as s from "./App.styles";
+
+// Components
+import Sidebar from "./components/Sidebar/Sidebar";
+import MainView from "./components/MainView/MainView";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <s.App>
+      <Sidebar />
+      <MainView />
+    </s.App>
   );
 }
 
---
+++ src/components/Sidebar/Sidebar.styles.js
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const SidebarContainer = styled.div`
+  width: 20%;
+  max-width: 280px;
+  min-width: 80px;
+  background: #545454;
+  //  background-image: linear-gradient(315deg, #8693ab 0%, #bdd4e7 74%);
+`;
